7ff6bbb490df106ec1b7edc2316ad0d2a99eb7e4
Luan Lee da Costa Faria <luan.faria.prestserv@petrobras.com.br>
2026-04-09 11:54:08 -0300

    feat: ajustando menu para de acordo com layout proposto

---
+++ src/app/components/header/header-menu.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sccon-header-menu',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, CommonModule],
+  templateUrl: './header-menu.component.html',
+  styleUrl: './header-menu.component.scss'
+})
+export class HeaderMenu {
+  menuAberto = false;
+
+  toggle() {
+    this.menuAberto = !this.menuAberto;
+  }
+
+  fecharMenu() {
+    this.menuAberto = false;
+  }
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { HeaderMenu } from './header-menu.component';
 
 @Component({
   selector: 'sccon-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule],
+  imports: [HeaderMenu],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class Header {
-  menuAberto = false;
+export class Header {}
 
-  toggle() {
-    this.menuAberto = !this.menuAberto;
-  }
-
-  fecharMenu() {
-    this.menuAberto = false;
-  }
-}
