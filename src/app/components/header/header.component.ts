6a8ab630b4bc655974a9277b354ac431ca1c884c
Luan Lee da Costa Faria <luan.faria.prestserv@petrobras.com.br>
2026-04-09 02:34:58 -0300

    feat: configurar Angular Material no projeto

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'sccon-header',
-  imports: [],
+  standalone: true,
+  imports: [MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
