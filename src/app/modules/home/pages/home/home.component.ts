d6c0a90be6c485263d36a9d3110c4563170a0725
Luan Lee da Costa Faria <luan.faria.prestserv@petrobras.com.br>
2026-04-10 00:36:00 -0300

    feat(menu): semibold no item ativo e refactor home.component

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,28 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'sccon-home',
-  standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class Home implements OnInit {
-  nomeDesenvolvedor = 'Luan Lee da Costa Faria';
-  dataAtual: string = '';
-
-  ngOnInit() {
-    this.formatarDataAtual();
-  }
-
-  private formatarDataAtual() {
-    const agora = new Date();
-    this.dataAtual = agora.toLocaleString('pt-BR', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric'
-    });
-  }
+export class Home {
+  readonly nomeDesenvolvedor = 'Luan Lee da Costa Faria';
+  readonly dataTeste = '10/04/2026';
 }
